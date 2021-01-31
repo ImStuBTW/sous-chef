9fb279bbb88b1e6a97d76bb1e1889537fd8fea85
Stuart Jones <Me@Stuart-Jones.com>
2021-01-31 10:18:50 -0600

    Enabled Twitch login logic.

--- src/Control/Config/Config.js
+++ src/Control/Config/Config.js
@@ -1,3 +1,6 @@
+// Import React packages.
+import { Component } from 'react';
+
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -6,26 +9,28 @@ import Twitch from './Twitch/Twitch';
 import OBS from './OBS/OBS';
 import Info from './Info/Info';
 
-function Config() {
-  return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <Twitch />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <OBS />
-        </Col>
-      </Row>
-      <Row>
-        <Col>
-          <Info />
-        </Col>
-      </Row>
-    </Container>
-  );
+class Config extends Component {
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col>
+            <Twitch socket={this.props.socket} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <OBS socket={this.props.socket} />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Info socket={this.props.socket} />
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default Config;
--- src/Control/Config/Twitch/Twitch.js
+++ src/Control/Config/Twitch/Twitch.js
@@ -1,3 +1,6 @@
+// Import React packages.
+import { Component } from 'react';
+
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
 import Col from 'react-bootstrap/Col';
@@ -8,29 +11,73 @@ import twitchIcon from '../../../Icons/twitch.png';
 
 import './twitch.scss';
 
-function Twitch(props) {
-  return (
-    <Card className="reminders-panel">
+class Twitch extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      status: false
+    }
+
+    this.handleBotConnect = this.handleBotConnect.bind(this);
+    this.handleLoginConnect = this.handleLoginConnect.bind(this);
+  }
+
+  componentDidMount() {
+    // Listen for updates to Twitch status.
+    this.props.socket.on('twitch-status', (msg) => {
+      this.setState({
+        status: msg.status
+      });
+    });
+
+    // When the component mounts, get the current Twitch status.
+    this.props.socket.emit('twitch-fetch', (msg) => {
+      this.setState({
+        status: msg.status
+      });
+    });
+  }
+
+  handleBotConnect() {
+    this.props.socket.emit('twitch-connect-bot', (msg) => {
+      console.log('Twitch.js | twitch-connect-bot | Twitich Bot Auth Status: ' + msg.status);
+      this.setState({
+        status: msg.status
+      });
+    })
+  }
+
+  handleLoginConnect() {
+    this.props.socket.emit('twitch-connect-owner', (msg) => {
+      console.log('Twitch.js | twitch-connect-owner | Twitich Broadcaster Auth Started.')
+    })
+  }
+
+  render() {
+    return (
+      <Card className="reminders-panel">
         <Card.Header><img src={twitchIcon} className="twitch-icon" alt="Twitch" />Twitch Integration</Card.Header>
         <Card.Body>
-            <Form>
-                <Form.Row>
-                    <Col>
-                        <Alert variant="success" className="twitch-alert">Twitch Authenticated Successfully.</Alert>
-                    </Col>
-                </Form.Row>
-            </Form>
-            <Form.Row className="twitch-buttons">
-                <Col>
-                    <Button variant="primary" block>Connect Bot to Twitch</Button>
-                </Col>
-                <Col>
-                    <Button variant="primary" block>Broadcaster Login</Button>
-                </Col>
+          <Form>
+            <Form.Row>
+              <Col>
+                <Alert variant="success" className="twitch-alert">Twitch Authenticated Successfully.</Alert>
+              </Col>
             </Form.Row>
+          </Form>
+          <Form.Row className="twitch-buttons">
+            <Col>
+              <Button variant="primary" block>Connect Bot to Twitch</Button>
+            </Col>
+            <Col>
+              <Button variant="primary" block>Broadcaster Login</Button>
+            </Col>
+          </Form.Row>
         </Card.Body>
-    </Card>
-  );
+      </Card>
+    );
+  }
 }
 
 export default Twitch;
