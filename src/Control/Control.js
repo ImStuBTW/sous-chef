96a14888499efe734b7d635e4c969e943ed0c2ae
Stuart Jones <me@stuart-jones.com>
2022-10-23 16:04:49 -0500

    Open auth callbacks on config tab.

--- src/Control/Control.js
+++ src/Control/Control.js
@@ -21,36 +21,53 @@ import Config from './Config/Config';
 // Open a new socket to our backend.
 let socket = io(`http://${window.location.hostname}:3001/`);
 
-// Tabs splits page into 3 main tabs.
-// Each tab is a component.
+
 class Control extends Component {
-componentDidMount() {
-  if(window.location.pathname) {
-    if(window.location.hash) {
-      const params = qs.parse(window.location.hash.substring(1));
-      if(params) {
-        if(params.access_token) {
-          if(window.location.pathname === '/botcallback') {
-            socket.emit('twitch-connect-bot-callback', params.access_token, (msg) => {
-              console.log(`Twitch.js | twitch-connect-bot-callback | Twitich Bot Auth Token : ${params.access_token}`);
-              this.setState({ botStatus: true });
-            });
-          }
-          if(window.location.pathname === '/ownercallback') {
-            socket.emit('twitch-connect-owner-callback', params.access_token, (msg) => {
-              console.log(`Twitch.js | twitch-connect-owner-callback | Twitich Bot Auth Token : ${params.access_token}`);
-              this.setState({ botStatus: true });
-            });
+  constructor(props) {
+    super(props);
+    this.state = {
+      key: "cook"
+    }
+  }
+
+  handleTab(key) {
+    this.setState({key: key});
+  }
+
+  // When the control panel loads, check and see if we're coming back from either of the two callback routes.
+  // This hook has an absurd amount of if tastements to make sure nothing that's acted upon is null.
+  // If you're not on the callbacks, the normal control panel page will fire without loading this.
+  componentDidMount() {
+    if(window.location.pathname) {
+      if(window.location.hash) {
+        const params = qs.parse(window.location.hash.substring(1));
+        if(params) {
+          if(params.access_token) {
+            if(window.location.pathname === '/botcallback') {
+              socket.emit('twitch-connect-bot-callback', params.access_token, (msg) => {
+                console.log(`Twitch.js | twitch-connect-bot-callback | Twitich Bot Auth Token : ${params.access_token}`);
+                this.setState({ botStatus: true });
+              });
+              this.handleTab('config');
+            }
+            if(window.location.pathname === '/ownercallback') {
+              socket.emit('twitch-connect-owner-callback', params.access_token, (msg) => {
+                console.log(`Twitch.js | twitch-connect-owner-callback | Twitich Bot Auth Token : ${params.access_token}`);
+                this.setState({ botStatus: true });
+              });
+              this.handleTab('config');
+            }
           }
         }
       }
     }
   }
-}
 
+// Tabs splits page into 3 main tabs.
+// Each tab is a component.
   render() {
     return (
-      <Tabs fill defaultActiveKey="cook" id="control-tabs" className="control-tabs">
+      <Tabs fill activeKey={this.state.key} onSelect={(key) => this.handleTab(key)} id="control-tabs" className="control-tabs">
         <Tab eventKey="cook" title="Cook">
           <Cook socket={socket} />
         </Tab>
